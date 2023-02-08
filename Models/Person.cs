a68ffc4f69224b683b8da794257f8422c6a6c928
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 10:46:37 +0800

    added functionality to rename property

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
 namespace Serializer.Models
 {
     public class Person
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        [JsonPropertyName("surname")] // to name the serialised property as something else
         public string? LastName { get; set; }
         public int Age { get; set; }
         public bool isAlive { get; set; }
