309b46a0f07f35b8e10a2e25fa56ccb4c1b047c4
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 09:03:17 +0800

    How to serialise a json object

---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+namespace Serializer.Models
+{
+    public class Person{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public bool isAlive { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Serializer.Models;
+
+// create an instance of the person model
+var person = new Person{
+    Id = 1,
+    Name = "John",
+    LastName = "Wick",
+    Age = 45,
+    isAlive = true
+};
+
+// serialize the object
+string jsonResult = JsonSerializer.Serialize(person);
+
+// place serialized object into JSON file
+var fileName = "person.json";
+File.WriteAllText(fileName, jsonResult);
