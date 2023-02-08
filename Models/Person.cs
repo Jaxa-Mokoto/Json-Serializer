45b06d582d6b31a6de7b36493f54e7eaa7eae554
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 10:50:22 +0800

    added conditional formatting

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,11 +4,15 @@ namespace Serializer.Models
     public class Person
     {
         public int Id { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] // to ignore the property with a condition
         public string? Name { get; set; }
 
-        [JsonPropertyName("surname")] // to name the serialised property as something else
+        [JsonPropertyName("surname")] // how to name the serialised property as something else
         public string? LastName { get; set; }
         public int Age { get; set; }
+
+        [JsonIgnore] // to exclude property from serialised object
         public bool isAlive { get; set; }
         public Address? Address { get; set; }
         public List<Phone>? Phones { get; set; }
